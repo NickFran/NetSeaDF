274aae426ef72db936246cb9dce5ad250243e889
NickFran <franciosi.nick@gmail.com>
2026-03-02 08:48:16 -0500

    .

---
+++ src/common/templates.js
@@ -0,0 +1,7 @@
+export const DataFileListItem = `
+<li class="DataFileListItem" id="DataFileListItem">
+    <img src="#" alt="ALT" class="DataFileIcon" id="DataFileIcon">
+    <h3 class="DataFileName" id="DataFileName"></h3>
+    <img src="#" alt="ALT" class="DataFileNotificationIcon" id="DataFileNotificationIcon">
+</li>
+`;// maybe we dont want a notif, how do we ever click it?
